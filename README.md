87f62f93a720996d92cf3e46ca56aa01cf6e141a
henriqueyun <henriqueyun@pop-os.localdomain>
2023-11-23 18:15:49 -0300

    feat: improving "interface" and added README.md

---
+++ README.md
@@ -0,0 +1,24 @@
+# Contador Peças Dominó
+
+Um assistente CLI para ajudar jogadores de dominó a realizarem uma contagem das peças. Foi uma das primeiras aplicações que seu desenvolvedor [henriqueyun](github.com/henriqueyun) desenvolveu utilizando javascript.
+
+## Utilizando
+Você pode rodar o assistente utilizando:
+```
+node index.js
+```
+
+Para utilizar o assistente basta informar qual foi a última jogada feita escrevendo os digitos que representam os valores da peça separados por um espaço:
+```
+Qual peça foi jogada?
+R: 1 1
+```
+
+A inserção deve ser feita **sempre do maior para o menor**, por exemplo para a peça quina-sena o formato da digitação deve ser `5 6` e não `6 5`.
+```
+Qual peça foi jogada?
+R: 1 2 // ✅
+
+Qual peça foi jogada?
+R: 2 1 // ❌
+```
--- index.js
+++ index.js
@@ -1,63 +1,80 @@
-const reader = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-
-var baralho = []
-
-function iniciarBaralho (){
-  baralho = []
-  for(let i = 0; i < 7 ; i++) {
-    for(let j = i; j < 7 ; j++) {
-      baralho.push([i,j]);
-    }
-  }
-}
-
-function imprimirBaralho (){
-  let menorNumero = baralho[0][0]
-  let baralhoString = ''
-  baralho.forEach((peca, i) => {
-    if (peca[0] > menorNumero) {
-      menorNumero = peca[0]
-      baralhoString += '\n'
-    }
-    baralhoString += peca + '  '
-  });
-  console.log(`\n${baralhoString}\n`)
-}
-
-function contarPecas (){
-  let naipes = [
-    {nome: 'liso', qtd: 0 },
-    {nome: 'piu', qtd: 0 },
-    {nome: 'duque', qtd: 0 },
-    {nome: 'terno', qtd: 0 },
-    {nome: 'quadra', qtd: 0 },
-    {nome: 'quina', qtd: 0 },
-    {nome: 'sena', qtd: 0 }
-  ]
-
-  // verifica todas as peças pra cada naipe
-  naipes.forEach((naipe, indiceNaipe) => {
-    baralho.forEach((peca, i) => {
-      if(peca.includes(indiceNaipe)) {
-        naipes[indiceNaipe].qtd++
-      }
+    console.clear()
+    const reader = require('readline').createInterface({
+      input: process.stdin,
+      output: process.stdout
     })
-    console.log(naipe.nome + ': ' + naipe.qtd)
-  })
-}
 
-function perguntarPeca (){
-  imprimirBaralho()
-  contarPecas()
-  reader.question('\nQual peça foi jogada?\nR: ', pecaJogada => {
-    console.log('out')
-    pecaJogada = JSON.parse(`[${pecaJogada.charAt(0)},${pecaJogada.charAt(2)}]`)
-    baralho = baralho.filter(peca => { return JSON.stringify(peca) != JSON.stringify(pecaJogada) })
-    perguntarPeca()
-  })
-}
+    let baralho = []
+    let mesa = []
+
+    function iniciarBaralho (){
+      baralho = []
+      for(let i = 0; i < 7 ; i++) {
+        for(let j = i; j < 7 ; j++) {
+          baralho.push([i,j]);
+        }
+      }
+    }
+
+    function imprimirBaralho (){
+      let menorNumero = baralho[0][0]
+      let baralhoString = ''
+      baralho.forEach((peca, i) => {
+        if (peca[0] > menorNumero) {
+          menorNumero = peca[0]
+          baralhoString += '\n'
+        }
+        baralhoString += peca + '  '
+      });
+      console.log("Contador Dominó CLI")
+      console.log(`\n${baralhoString}\n`)
+    }
+
+    function contarPecas () {
+      let naipes = [
+        {nome: 'liso', qtd: 0 },
+        {nome: 'piu', qtd: 0 },
+        {nome: 'duque', qtd: 0 },
+        {nome: 'terno', qtd: 0 },
+        {nome: 'quadra', qtd: 0 },
+        {nome: 'quina', qtd: 0 },
+        {nome: 'sena', qtd: 0 }
+      ]
+      // verifica todas as peças pra cada naipe
+      naipes.forEach((naipe, indiceNaipe) => {
+            baralho.forEach((peca) => {
+            if(peca.includes(indiceNaipe)) {
+                naipes[indiceNaipe].qtd++
+            }
+        })
+        console.log(naipe.nome + ': ' + naipe.qtd)
+      })
+    }
+
+    function perguntarPeca (msg){
+      console.clear()
+      if (msg) {
+        console.error(`\x1b[1;31m${msg}\x1b[22;37m`)
+      }
+      imprimirBaralho()
+      contarPecas()
+
+      reader.question('\nQual peça foi jogada?\nR: ', pecaJogada => {
+        if (pecaJogada === "sair") {
+            process.exit(1)
+        }
+
+        if (mesa.includes(pecaJogada)) {
+            perguntarPeca("Essa peça já foi jogada!")
+            return
+        }
+
+        mesa.push(pecaJogada)
+        pecaJogada = JSON.parse(`[${pecaJogada.charAt(0)},${pecaJogada.charAt(2)}]`)
+        baralho = baralho.filter(peca => { return JSON.stringify(peca) != JSON.stringify(pecaJogada) })
+        perguntarPeca()
+      })
+    }
+
 iniciarBaralho()
 perguntarPeca()
